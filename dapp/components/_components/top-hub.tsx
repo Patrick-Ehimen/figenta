731f0d6760b66490c4a092d42b102240a51715df
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-11-15 18:09:05 +0100

    add image assets

--- dapp/components/_components/top-hub.tsx
+++ dapp/components/_components/top-hub.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import EthHub from "./eth-hub";
+import BnbHub from "./bnb-hub";
+import AvalancheHub from "./avalanche-hub";
 
 export default function TopHub() {
-  return <div>top-hub</div>;
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-6">Top Cross-Chain Hub</h1>
+      <div className="grid gap-4">
+        <div className="grid grid-cols-5 gap-4 text-sm text-gray-500 px-4">
+          <div>Token Pool</div>
+          <div>Rewards</div>
+          <div>TVL</div>
+          <div>24h Vol</div>
+          <div>Highest APR</div>
+        </div>
+        <EthHub />
+        <BnbHub />
+        <AvalancheHub />
+      </div>
+    </div>
+  );
 }
--- dapp/public/index.ts
+++ dapp/public/index.ts
@@ -1,3 +1,8 @@
 import Logo from "./assets/logo.svg";
+import Avalanche from "./assets/avalanche.svg";
+import BNB from "./assets/bnb.svg";
+import Ethereum from "./assets/ethereum.svg";
+import Polygon from "./assets/polygon.svg";
+import LogoIcon from "../app/icon.svg";
 
-export { Logo };
+export { Logo, Avalanche, BNB, Ethereum, Polygon, LogoIcon };
